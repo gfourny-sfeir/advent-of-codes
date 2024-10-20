cd5e9fb664a0a93929fe587f4a0f92fddd4fd77e
Guillaume Fourny <fourny.g@sfeir.com>
2024-10-20 21:23:30 +0200

    Update ValueExtractor.java

--- 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
+++ 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
@@ -1,15 +1,13 @@
 package fr.adventofcodes;
 
-import java.util.function.BiFunction;
-
 public class ValueExtractor {
 
-    private static char getLastDigit(String digits) {
-        return digits.charAt(digits.length() - 1);
+    private static String getLastDigit(String digits) {
+        return String.valueOf(digits.charAt(digits.length() - 1));
     }
 
-    private static char getFirstDigit(String digits) {
-        return digits.charAt(0);
+    private static String getFirstDigit(String digits) {
+        return String.valueOf(digits.charAt(0));
     }
 
     public int extract(String string) {
@@ -27,8 +25,6 @@ public class ValueExtractor {
         var firstDigit = getFirstDigit(digits);
         var lastDigit = getLastDigit(digits);
 
-        BiFunction<Character, Character, String> toString = (first, last) -> String.valueOf(first) + last;
-
-        return Integer.parseInt(toString.apply(firstDigit, lastDigit));
+        return Integer.parseInt(firstDigit.concat(lastDigit));
     }
 }
