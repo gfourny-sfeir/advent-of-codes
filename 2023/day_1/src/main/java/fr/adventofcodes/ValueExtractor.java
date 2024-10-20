011bc8b67238d523e4b5227a6510a6e5ff31d67d
Guillaume Fourny <fourny.g@sfeir.com>
2024-10-20 18:37:18 +0200

    part1 ok

--- 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
+++ 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
@@ -1,33 +1,34 @@
 package fr.adventofcodes;
 
-import static java.lang.Character.isDigit;
-import static java.util.Objects.isNull;
+import java.util.function.BiFunction;
 
 public class ValueExtractor {
 
+    private static char getLastDigit(String digits) {
+        return digits.charAt(digits.length() - 1);
+    }
+
+    private static char getFirstDigit(String digits) {
+        return digits.charAt(0);
+    }
+
     public int extract(String string) {
 
-        String firstNumber = null;
-        String lastNumber = null;
+        var digits = string.chars()
+                .mapToObj(ch -> (char) ch)
+                .filter(Character::isDigit)
+                .map(String::valueOf)
+                .reduce("", String::concat);
 
-        for (char c : string.toCharArray()) {
-
-            if (isDigit(c)) {
-
-                if (isNull(firstNumber)) {
-                    firstNumber = String.valueOf(c);
-                }
-
-                if (c != firstNumber.charAt(0)) {
-                    lastNumber = String.valueOf(c);
-                }
-            }
+        if (digits.isEmpty()) {
+            return 0;
         }
 
-        if (isNull(lastNumber)) {
-            lastNumber = firstNumber;
-        }
+        var firstDigit = getFirstDigit(digits);
+        var lastDigit = getLastDigit(digits);
 
-        return Integer.parseInt(firstNumber + lastNumber);
+        BiFunction<Character, Character, String> toString = (first, last) -> String.valueOf(first) + last;
+
+        return Integer.parseInt(toString.apply(firstDigit, lastDigit));
     }
 }
