e4a12b2e321d70a9cd861291f7223414c78e5a97
Guillaume Fourny <fourny.g@sfeir.com>
2024-10-21 09:09:07 +0200

    feat: Part 2

--- 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
+++ 2023/day_1/src/main/java/fr/adventofcodes/ValueExtractor.java
@@ -1,6 +1,23 @@
 package fr.adventofcodes;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ValueExtractor {
+
+    private final LinkedHashMap<String, String> textNumbers = new LinkedHashMap<>(
+            Map.of(
+                    "one", "one1one",
+                    "two", "two2two",
+                    "three", "three3three",
+                    "four", "four4four",
+                    "five", "five5five",
+                    "six", "six6six",
+                    "seven", "seven7seven",
+                    "eight", "eight8eight",
+                    "nine", "nine9nine"
+            )
+    );
 
     private static String getLastDigit(String digits) {
         return String.valueOf(digits.charAt(digits.length() - 1));
@@ -26,5 +43,14 @@ public class ValueExtractor {
         var lastDigit = getLastDigit(digits);
 
         return Integer.parseInt(firstDigit.concat(lastDigit));
+    }
+
+    public int extractWithText(String string) {
+        for (var entry : textNumbers.entrySet()) {
+            if (string.contains(entry.getKey())) {
+                string = string.replace(entry.getKey(), entry.getValue());
+            }
+        }
+        return extract(string);
     }
 }
