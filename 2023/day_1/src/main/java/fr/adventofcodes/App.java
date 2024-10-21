0ab24f0b6d4586a578c6c6f8e2e5e93d730980b3
Guillaume Fourny <125885296+gfourny-sfeir@users.noreply.github.com>
2024-10-21 09:09:29 +0200

    feat: Part 2 (#2)

--- 2023/day_1/src/main/java/fr/adventofcodes/App.java
+++ 2023/day_1/src/main/java/fr/adventofcodes/App.java
@@ -10,10 +10,18 @@ public class App {
         var url = App.class.getClassLoader().getResource("input.txt").getFile();
         var extractor = new ValueExtractor();
 
-        try (var lines = Files.lines(Path.of(url))) {
-            var reduce = lines.mapToInt(extractor::extract).sum();
+        Path path = Path.of(url);
 
-            System.out.println(reduce);
+        try (var lines = Files.lines(path)) {
+            var partOneSum = lines.mapToInt(extractor::extract).sum();
+
+            System.out.println("part 1 result: " + partOneSum);
+        }
+
+        try (var lines = Files.lines(path)) {
+            var partTwoSum = lines.mapToInt(extractor::extractWithText).sum();
+
+            System.out.println("part 2 result: " + partTwoSum);
         }
     }
 }
--- 2023/day_1/src/test/java/fr/adventofcodes/ValueExtractorTest.java
+++ 2023/day_1/src/test/java/fr/adventofcodes/ValueExtractorTest.java
@@ -7,6 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ValueExtractorTest {
 
+    private final ValueExtractor extractor = new ValueExtractor();
+
     @ParameterizedTest
     @CsvSource(delimiter = '|', textBlock = """
             1abc2                 | 12
@@ -18,9 +20,24 @@ class ValueExtractorTest {
             """)
     void should_correctly_get_numbers(String string, int value) {
 
-        var extractor = new ValueExtractor();
-
         var result = extractor.extract(string);
+
+        assertThat(result).isEqualTo(value);
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', textBlock = """
+            two1nine         | 29
+            eightwothree     | 83
+            abcone2threexyz  | 13
+            xtwone3four      | 24
+            4nineeightseven2 | 42
+            zoneight234      | 14
+            7pqrstsixteen    | 76
+            """)
+    void should_find_with_text(String string, int value) {
+
+        var result = extractor.extractWithText(string);
 
         assertThat(result).isEqualTo(value);
     }
